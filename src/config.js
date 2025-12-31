bc552d464874f802b56025ac89e9a5b68b8fc210
avecoder avecoder <avecoderavecoder@MacBook-Air-avecoder-2.local>
2025-12-31 07:08:29 +0300

    test

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 // Меняй значения здесь.
 
 export const MOCKAPI_BASE_URL = 'https://695492241cd5294d2c7ce7b0.mockapi.io'
-export const MOCKAPI_RESOURCE = 'postcard'
+export const MOCKAPI_RESOURCE = 'postcard/cards'
 
 // 1 = показывать iq.svg на лицевой стороне (слева сверху)
 export const IQ_ENABLED = false
